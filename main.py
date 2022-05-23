94ff924e616710dfb84c6391bbe9b98503b57b8b
teemoleo <105948510+teemoleo@users.noreply.github.com>
2022-05-23 13:43:12 +0100

    Update and rename mykivy.py to main.py

--- main.py
+++ main.py
@@ -447,7 +447,7 @@ class GenderSelection(Screen):
 class WindowManager(ScreenManager):
     pass
 
-kv = Builder.load_file("mykivy.kv")
+kv = Builder.load_file("main.kv")
 
 class MainApp(App):
     def build(self):
